36c5cdea45e77da3664dd30dfffe486564d17a65
Oldrick <o.weenink@hotmail.com>
2022-03-22 18:53:16 +0100

    working?

--- calculator.js
+++ calculator.js
@@ -18,9 +18,10 @@ const operate = (operator, a, b) => {
     {
         return multiply(a,b)
     }
-    else if (operate == '/'){
+    else if (operator == '/'){
         return divide(a,b)
     }
+    console.log("operate() executed")
 };
 // console.log(operate('*', 5,2));
 // works
@@ -50,8 +51,12 @@ resultButton.addEventListener('click', () => {
     result = displayValue.split(''); // step 1. string to array
     result = concat(result); // step 2. concat integers if next to another
     result = minEval(result)// step 3. Evaluate for minus signs, -+ > -, if leading '-' in front of number > ,-number, 
-
-
+    let remnant;
+    arrayWithRemnantAndResult = checkRemnant(result)// step 4. Check for trailing operator. Store it for next calculation
+    remnant = arrayWithRemnantAndResult[0];
+    result = arrayWithRemnantAndResult[1];
+    console.log(remnant, result); // store the value of remnant in a new global 'toBeCalculated' after result is done.
+    finalResult = evaluate(result); // TODO
     displayValue = '' // reset display value
 })
 
@@ -90,12 +95,38 @@ function concat(arr){
     {
         newArr.pop();
     }
+    console.log("concat:", newArr);
     return newArr;
 }
 
-function minEval(arr){ // ----------------------WORKS!--------------------
+function minEval(arr){ // ----------------------buggy with rare cornercases--------------------
+    let mincount =0;
+    let tempArray = [];
+    arr.push('');
+    arr.forEach((item, index, arr) => {
+        if (item == '-')
+        {
+            mincount++;
+            return;
+        }
+        else {
+            if (mincount > 0)
+                {
+                    if (mincount % 2 == 0){
+                        tempArray.push('+');
+                        } else {
+                            tempArray.push('-');
+                        }
+                }
+            tempArray.push(item);
+            mincount = 0; 
+        }
+    })
+    console.log("tempArray: ", tempArray);
+    tempArray.pop();
+    arr = tempArray;
+    
     let newArr = [];
-    console.log(arr);
     let joinedNumber = ''
     let count = 0, fcount = 0;
     arr.forEach((item, index, arr) => {
@@ -110,13 +141,18 @@ function minEval(arr){ // ----------------------WORKS!--------------------
         {
             console.log("SYNTAX ERROR")
         }
-        if (count == 0 && (item == '-') || (item == '+'))
+        if (count == 0 && ((item == '-') || (item == '+')))
         {
+            console.log(item)
             joinedNumber = (arr.slice(index, index+2)).join('');
             newArr.push(joinedNumber);
-            console.log(newArr);
             count++
+            arr.splice(index,1);
             return;
+        }
+        if (Number.isInteger(parseInt(item)))
+        {
+            newArr.push(item);
         }
         if (!Number.isInteger(parseInt(item)))
         {
@@ -124,8 +160,85 @@ function minEval(arr){ // ----------------------WORKS!--------------------
             count = 0; 
         }
     });
-    console.log(newArr);
+    console.log("minEval:", newArr);
+    return newArr;
+}
 
+function checkRemnant(arr){ //------------------------works----------
+    let len = arr.length;
+    let last = arr[len-1];
+    if (!Number.isInteger(parseInt(last)))
+    {
+        arr.pop();
+        return [last, arr];
+    }
+    else{
+        return [undefined, arr];
+    }
+    
+}
+
+function evaluate(arr) {
+    let partialAnswer = 0; // trusting on a valid input
+    let tempArray = [];
+    // check if just a number is entered:
+    let len = arr.length;
+    if (len == 1)
+    {
+        return arr[0];
+    }
+    console.log(arr);
+    while (arr.includes('/')) {
+        for(let i = 0; i < arr.length; i++){
+            if (arr[i] == '/') {
+                partialAnswer = operate(arr[i],parseInt(arr[i - 1]), parseInt(arr[i + 1]));
+                tempArray.push(partialAnswer);
+                arr = tempArray.concat(arr.slice(i+2));
+                console.log("new arr = -------",arr);
+                break;
+            }
+        }
+    }
+        
+    console.log(partialAnswer);
+    while (arr.includes('*')) {
+        for(let i = 0; i < arr.length; i++){
+            if (arr[i] == '*') {
+                partialAnswer = operate(arr[i],parseInt(arr[i - 1]), parseInt(arr[i + 1]));
+                tempArray.push(partialAnswer);
+                arr = tempArray.concat(arr.slice(i+2));
+                console.log("new arr = -------",arr);
+                break;
+            }
+        }
+    }
+    console.log(partialAnswer);
+    while (arr.includes('-')) {
+        for(let i = 0; i < arr.length; i++){
+            if (arr[i] == '-') {
+                partialAnswer = operate(arr[i],parseInt(arr[i - 1]), parseInt(arr[i + 1]));
+                tempArray.push(partialAnswer);
+                arr = tempArray.concat(arr.slice(i+2));
+                console.log("new arr = -------",arr);
+                break;
+            }
+        }
+    }
+    console.log(partialAnswer);
+    while (arr.includes('+')) {
+        for(let i = 0; i < arr.length; i++){
+            if (arr[i] == '+') {
+                partialAnswer = operate(arr[i],parseInt(arr[i - 1]), parseInt(arr[i + 1]));
+                tempArray.push(partialAnswer);
+                arr = tempArray.concat(arr.slice(i+2));
+                console.log("new arr = -------",arr);
+                break;
+            }
+        }
+    }
+    console.log(partialAnswer);
+    return partialAnswer;
+    
 }
 
 
