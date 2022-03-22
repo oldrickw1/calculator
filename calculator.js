a11dc8623b40416f0e69717c9da57f4b99555b3c
Oldrick <o.weenink@hotmail.com>
2022-03-22 14:48:06 +0100

    implemented minEval

--- calculator.js
+++ calculator.js
@@ -48,20 +48,86 @@ const resultButton = document.querySelector('button.result');
 
 resultButton.addEventListener('click', () => {
     result = displayValue.split(''); // step 1. string to array
-    len = result.length;
-    calculatedResult = 0; 
-    // step 2. concat integers if next to another
-    console.log(result);
-    result = concat(result);
-    //console.log(result)
-
-
-
+    result = concat(result); // step 2. concat integers if next to another
+    result = minEval(result)// step 3. Evaluate for minus signs, -+ > -, if leading '-' in front of number > ,-number, 
 
 
     displayValue = '' // reset display value
-    display.innerHTML = calculatedResult;
 })
+
+
+// concatenate number function -----------WORKS!--------------------------------------
+function concat(arr){
+    let len = arr.length;
+    // add extra element (used for iteration)
+    len = arr.push('');
+    let count = 0; 
+    let joinedNumber = []
+    let newArr = []
+    let bool = false;
+    arr.forEach((item, index, arr) => {
+        if (Number.isInteger(parseInt(item))){
+            count++;
+        }
+        if (count > 1 && (!Number.isInteger(parseInt(item)))) // if current item is not a number, check if counter went up. If so, join the previous numbers together.
+        {
+            joinedNumber = arr.slice(index-count, index).join(''); // 
+            newArr.push(joinedNumber); // add combined number
+            count = 0;
+            count = 0;
+        }
+        if (count == 1 && (!Number.isInteger(parseInt(item))))
+        {
+            newArr.push(arr[index-1]);
+            count = 0; 
+        }
+        if ((!Number.isInteger(parseInt(item)))){
+            newArr.push(item);
+        }
+    })
+    len = newArr.length;
+    if (newArr[len-1] == '')
+    {
+        newArr.pop();
+    }
+    return newArr;
+}
+
+function minEval(arr){ // ----------------------WORKS!--------------------
+    let newArr = [];
+    console.log(arr);
+    let joinedNumber = ''
+    let count = 0, fcount = 0;
+    arr.forEach((item, index, arr) => {
+        if (Number.isInteger(parseInt(item))){
+            count++;
+            fcount = 0; 
+        }
+        else {
+            fcount++;
+        }
+        if (fcount > 2)
+        {
+            console.log("SYNTAX ERROR")
+        }
+        if (count == 0 && (item == '-') || (item == '+'))
+        {
+            joinedNumber = (arr.slice(index, index+2)).join('');
+            newArr.push(joinedNumber);
+            console.log(newArr);
+            count++
+            return;
+        }
+        if (!Number.isInteger(parseInt(item)))
+        {
+            newArr.push(item);
+            count = 0; 
+        }
+    });
+    console.log(newArr);
+
+}
+
 
 // clear display
 const clearButton = document.querySelector('button.clear');
@@ -80,33 +146,3 @@ delButton.addEventListener('click', () => {
     displayValue = temp.join('');
     display.innerHTML = displayValue;
 })
-
-// concatenate number function
-function concat(arr){
-    let len = arr.length;
-    // add extra element (used for iteration)
-    len = arr.push('');
-    let count = 0; 
-    let joinedNumber = []
-    console.log(arr)
-    let newArr = []
-    let bool = false;
-    arr.forEach((i, index, arr) => {
-        if (Number.isInteger((parseInt(i)))) // if the item is a number, the counter goes up for each consecutive int
-        {
-            count++;
-        }
-        else if (count != 0) // if current item is not a number, check if counter went up. If so, join the previous numbers together.
-        {
-            joinedNumber = arr.slice(index-count, index).join(''); // 
-            console.log(joinedNumber);
-            newArr.push(joinedNumber); // add combined number
-            newArr.push(i); // add operator
-            count = 0;
-            console.log(newArr);
-        }
-    })
-    newArr.pop();
-    console.log(newArr);
-}
-
